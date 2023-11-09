50151dbdf9761d43fd0f6d97391d5ebd72cfa481
Bharath Vishnu B <bharathvb1204@gmail.com>
2023-11-09 22:09:11 +0530

    font-poppins added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from './components/Home';
 
 function App() {
   return (
-    <div className='min-h-screen flex flex-col'>
+    <div className='min-h-screen flex flex-col font-poppins'>
       <Home/>
     </div>
   )
