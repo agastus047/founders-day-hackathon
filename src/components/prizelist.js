1f8093821716bc27ae32ab1a12ae3e6b09af10c3
Agastus Joyson <agastusjoyson@gmail.com>
2023-11-10 22:13:35 +0530

    price list changes

--- src/components/prizelist.js
+++ src/components/prizelist.js
@@ -6,35 +6,38 @@ const prizelist =[
     id:0,
     iconlink:icon,
     title:'First Prize',
-    prize:'₹30000',
+    prize:'₹1 lakh',
  },
  {
     id:1,
     iconlink:icon,
     title:'Second Prize',
-    prize:'₹15000',
+    prize:'₹50000',
  },
  {
     id:2,
     iconlink:icon,
     title:'Third Prize',
-    prize:'₹7500',
+    prize:'₹30000',
  },
  {
     id:3,
     iconlink:best,
     title:'Best Software',
-    prize:'₹10000',
+    prize:'₹30000',
  },
  {
     id:4,
     iconlink:best,
     title:'Best Hardware',
-    prize:'₹10000',
+    prize:'₹30000',
  },
- 
-
-
+ {
+    id:5,
+    iconlink:best,
+    title:'Track 1,2,3',
+    prize:'₹10000 each',
+ },
 ]
 
 export default prizelist
