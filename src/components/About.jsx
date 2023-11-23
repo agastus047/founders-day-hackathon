540fc2384c3b723ffa37e6325a20e4fd1e967c76
Muhammed <84435599+muhammed-mizaj@users.noreply.github.com>
2023-11-23 21:28:50 +0530

    changed gradient color

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () =>
         <div data-aos="fade" className="w-full  text-white lg:px-20 px-4 py-10 mt-6">
         <div className="flex bg-gray-800 w-full flex-col lg:flex-row justify-around h-full">
             <div className=" text-left flex flex-col h-full w-full lg:w-1/2 p-10 gap-5">
-                <h1 className="font-bold text-5xl mb-2 underline underline-offset-8 decoration-dashed">About <span className='bg-gradient-to-b from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent'>Hack4TKM</span></h1>
+                <h1 className="font-bold text-5xl mb-2 underline underline-offset-8 decoration-dashed">About <span className='bg-gradient-to-b from-blue-500 to-light-blue-500 bg-clip-text text-transparent'>Hack4TKM</span></h1>
                 <p className="text-start text-xl leading-relaxed">
                 Amidst the celebration of TKM College of Engineering's Founder’s Day 2024, 
                 get ready to ignite the spark of innovation with Hack4TKM – a 
