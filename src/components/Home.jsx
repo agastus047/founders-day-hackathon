fe9a024ac6e293db0c6c46684dd81af644c53db3
muhammed-mizaj <mdprgamer@gmail.com>
2023-11-29 14:35:54 +0530

    minor changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -75,7 +75,7 @@ const Home = () => {
                     <div className="flex justify-center items-center">
         <div className="apply-button" 
             data-hackathon-slug="hack4tkm" 
-            data-button-theme="light"
+            data-button-theme="dark"
             style={{height: 44, width: 312}}
         ></div>
     </div>
