dc055b8b3ca487bb5f1c4dd2d06c6d12c7f009fe
muhammed-mizaj <mdprgamer@gmail.com>
2023-11-29 15:31:44 +0530

    minor changes

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -5,9 +5,9 @@ const Container = () => {
   return (
     <div data-aos='fade-up' className="bg-black mb-5 md:mt-10 md:mb-10 flex flex-col md:flex-row gap-5 md:gap-20 justify-center items-center">
       <div className=" w-full md:w-auto p-4">
-        <div className="text-white p-2">
-          <p className="text-shadows font-bold text-center  text-transparent bg-gradient-to-t from-[#1b0362] to-[#50fefe] bg-clip-text text-5xl md:text-5xl lg:text-7xl">36 HOURS</p>
-          <p className='mt-3 md:mt-5 font-medium text-center text-lg md:text-2xl'>OF NONSTOP HACKING</p>
+        <div className=" p-2">
+        <p className="text-shadows font-bold text-center  text-transparent bg-gradient-to-t from-[#1b0362] to-[#50fefe] bg-clip-text text-5xl md:text-5xl lg:text-7xl">36 HOURS</p>
+          <p className='text-white mt-3 md:mt-5 font-medium text-center text-lg md:text-2xl'>OF NONSTOP HACKING</p>
         </div>
       </div>
       <div className="w-full md:w-auto p-4">
