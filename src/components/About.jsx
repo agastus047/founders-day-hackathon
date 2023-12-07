a9a57e0ceb8336fe43053cf829116f3b7c675a9c
Yadhukrishnan <ybc2204@gmail.com>
2023-12-07 22:28:43 +0530

    mobile view set

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () =>
         <div data-aos="fade" className="w-full bg-black  text-white lg:px-20 px-6 py-6 mt-6 ">
         <div className="flex  w-full flex-col lg:flex-row justify-around h-full bg-black border-4 border-gradient-animation p-4 rounded-lg">
             <div className=" text-left flex flex-col h-full w-full lg:w-1/2 p-5 md:p-10 gap-5">
-                <h1 className="font-bold text-5xl mb-2 underline underline-offset-8 decoration-dashed leading-snug">About <span className=' text-transparent bg-gradient-to-b from-[#1b0362] to-[#50fefe] bg-clip-text'>HACK4TKM</span></h1>
+                <h1 className="font-bold text-4xl lg:text-5xl mb-2 underline underline-offset-8 decoration-dashed leading-snug">About <span className=' text-transparent bg-gradient-to-b from-[#1b0362] to-[#50fefe] bg-clip-text'>HACK4TKM</span></h1>
                 <p className=" text-xl  ">
                 Amidst the celebration of Founder’s Day 2024, get ready to
     ignite the spark of innovation with HACK4TKM – a national-level hackathon, organized by TKM College of Engineering (TKMCE),
