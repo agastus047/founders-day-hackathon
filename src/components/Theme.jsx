3329c9927f63ac5e49fd8781d346cfe4f26b352b
muhammed-mizaj <mdprgamer@gmail.com>
2023-12-11 19:27:23 +0530

    theme countdown added

--- src/components/Theme.jsx
+++ src/components/Theme.jsx
@@ -1,14 +1,51 @@
-import './text.css';
-const Theme = () => {
-  
-  return (
-    <div className="flex justify-center items-center flex-col my-8 gap-3 mx-5">
-    <div className="text-white font-bold text-4xl md:text-5xl text-center my-5   ">THEME</div>
-     <div className="mt-5 text-3xl lg:text-5xl bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text font-extrabold h-max uppercase text-center font-jost">
-        <p className="text-transparent bg-gradient-to-l from-[#1b0362] to-[#50fefe] bg-clip-text text-shadows leading-relaxed" >Smart and Sustainable Designs for Agriculture, Food and Rural Development.</p>
-      </div>   
-    </div>
-  )
-}
+import React, { useState, useEffect } from 'react';
 
-export default Theme
+const Theme = () => {
+  const [countdown, setCountdown] = useState({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+  const [showCountdown, setShowCountdown] = useState(true);
+
+  useEffect(() => {
+    const deadline = new Date('December 15, 2023 00:00:00').getTime();
+
+    const updateCountdown = () => {
+      const now = new Date().getTime();
+      const distance = deadline - now;
+
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      setCountdown({ days, hours, minutes, seconds });
+
+      // If the deadline has passed, stop the countdown and show "Coming Soon" text
+      if (distance <= 0) {
+        setShowCountdown(false);
+      }
+    };
+
+    const interval = setInterval(updateCountdown, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="my-8 mx-5">
+      <div className="text-white font-bold text-4xl md:text-5xl text-center my-5">THEME</div>
+      <div className="mt-5 text-xl lg:text-3xl bg-clip-text font-extrabold h-max uppercase text-center">
+        {showCountdown ? (
+          <div>
+             <p className="text-transparent bg-gradient-to-t from-[#1b0362] to-[#50fefe] bg-clip-text leading-relaxed">
+            {countdown.days} days: {countdown.hours} hours: {countdown.minutes} minutes: {countdown.seconds} seconds
+          </p>
+            </div>
+         
+        ) : (
+          <p className="text-white">Coming Soon</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Theme;
