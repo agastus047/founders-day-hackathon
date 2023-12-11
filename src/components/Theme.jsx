d4f7e1a037510f2d9c9b0a77b0dd78c1c8f15363
Bharath Vishnu B <bharathvb1204@gmail.com>
2023-12-11 18:43:18 +0530

    theme div changed

--- src/components/Theme.jsx
+++ src/components/Theme.jsx
@@ -2,9 +2,9 @@ import './text.css';
 const Theme = () => {
   
   return (
-    <div className="flex justify-center items-center flex-col my-8 gap-3 mx-5">
-    <div className="text-white font-bold text-4xl md:text-5xl text-center my-5   ">THEME</div>
-     <div className="mt-5 text-3xl lg:text-5xl bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text font-extrabold h-max uppercase text-center font-jost">
+    <div className="flex justify-center items-center flex-col my-8 gap-3 mx-5 border-4 border-gradient-animation  mr-6 ml-6 md:mr-20 md:ml-20">
+    <div className="text-white font-bold text-4xl md:text-5xl text-center my-5">THEME</div>
+     <div className="mt-5 text-3xl lg:text-5xl bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text font-extrabold h-max uppercase text-center font-jost mb-20 ">
         <p className="text-transparent bg-gradient-to-l from-[#1b0362] to-[#50fefe] bg-clip-text text-shadows leading-relaxed" >Smart and Sustainable Designs for Agriculture, Food and Rural Development.</p>
       </div>   
     </div>
