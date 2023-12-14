1f714279c2a97f036135305d321872f84f2601d6
muhammed-mizaj <mdprgamer@gmail.com>
2023-12-14 20:13:43 +0530

    minor changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () =>
         <div data-aos="fade" className="w-full bg-black  text-white lg:px-20 px-6 py-6 mt-6 ">
         <div className="flex  w-full flex-col lg:flex-row justify-around h-full bg-black border-4 border-gradient-animation p-2 rounded-lg">
             <div className=" text-left flex flex-col h-full w-full lg:w-1/2 p-5 md:p-10 gap-5">
-                <h1 className="font-bold text-4xl lg:text-5xl mb-2 underline underline-offset-8 decoration-dashed leading-snug">About <span className=' text-transparent bg-gradient-to-l from-[#148ef9] to-[#50feff] bg-clip-text'>HACK4TKM</span></h1>
+                <h1 className="font-bold text-4xl lg:text-5xl mb-2 underline underline-offset-8 decoration-dashed leading-snug">About <span className=' text-transparent bg-gradient-to-b from-[#148ef9] to-[#50feff] bg-clip-text'>HACK4TKM</span></h1>
                 <p className=" text-2xl  ">
                     
                 Amidst the celebration of Founder’s Day 2024, get ready to
