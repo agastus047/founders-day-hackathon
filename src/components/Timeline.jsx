679f45e2737be9bc6da3406e347952be9ded031f
muhammed-mizaj <mdprgamer@gmail.com>
2023-12-23 13:42:44 +0530

    minor changes

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -15,13 +15,13 @@ const CustomizedTimeline = () => {
         <div data-aos="fade" className="col-span-4 w-full h-full ">
           <div className="w-full h-full bg-black border border-white p-4 rounded-md p-2 py-4 md:pl-4 border border-gradient-animation [#148ef9]">
             <h1 className="text-white text-xl text-center font-medium py-2 ">15 December 2023</h1>
-            <p className="text-gray-100 sm:text-xl text-md text-center mb-2">Abstract Submission .</p>
+            <p className="text-gray-100 sm:text-xl text-md text-center mb-2">Abstract Submission </p>
           </div>
         </div>
         <div data-aos="fade" className="col-span-4 w-full h-full">
           <div className="w-auto h-auto bg-black border border-white p-4 rounded-xl p-2 py-4 md:pl-6 border border-gradient-animation [#50fefe]">
             <h1 className="text-white text-center text-xl font-medium py-2">15 January 2024</h1>
-            <p className="text-gray-100 text-center sm:text-xl text-md mb-2">Abstract Submission Ends.</p>
+            <p className="text-gray-100 text-center sm:text-xl text-md mb-2">Abstract Submission Ends </p>
           </div>
         </div>
         <div className="relative col-span-1 w-full h-full flex justify-center items-center">
@@ -38,13 +38,13 @@ const CustomizedTimeline = () => {
         <div data-aos="fade" className="col-span-4 w-full h-full ">
           <div className="w-full h-full bg-black border border-white p-4 rounded-md p-2 py-4 md:pl-4 border border-gradient-animation [#148ef9]">
             <h1 className="text-white text-xl text-center font-medium py-2">20 January 2024</h1>
-            <p className="text-gray-100 sm:text-xl text-md text-center mb-2">Announcement of Shortlisted 20 Teams.</p>
+            <p className="text-gray-100 sm:text-xl text-md text-center mb-2">Announcement of Shortlisted 20 Teams </p>
           </div>
         </div>
         <div data-aos="fade" className="col-span-4 w-full h-full ">
           <div className="w-full h-full bg-black border border-white p-4 rounded-md p-2 py-4 md:pl-4 border border-gradient-animation [#50fefe]">
             <h1 className="text-white text-xl text-center font-medium py-2">16 February 2024</h1>
-            <p className="text-gray-100 text-center sm:text-xl text-md mb-2">Reporting at the Venue.</p>
+            <p className="text-gray-100 text-center sm:text-xl text-md mb-2">Reporting at the Venue </p>
           </div>
         </div>
 
@@ -61,13 +61,13 @@ const CustomizedTimeline = () => {
         <div data-aos="fade" className="col-span-4 w-full h-full ">
           <div className="w-full h-full bg-black border border-white p-4 rounded-md p-2 py-4 md:pl-4 border border-gradient-animation [#148ef9]">
             <h1 className="text-white text-xl text-center font-medium py-2">17 February 2024</h1>
-            <p className="text-gray-100 text-center sm:text-xl text-md mb-2">Hackathon Day-1</p>
+            <p className="text-gray-100 text-center sm:text-xl text-md mb-2">Hackathon Day-1 </p>
           </div>
         </div>
         <div data-aos="fade" className="col-span-4 w-full h-full ">
           <div className="w-full h-full bg-black border border-white p-4 rounded-md p-2 py-4 md:pl-4 border border-gradient-animation [#50fefe]">
             <h1 className="text-white text-xl text-center font-medium py-2">18 February 2024</h1>
-            <p className="text-gray-100 sm:text-xl text-md text-center mb-2">Hackathon Day-2</p>
+            <p className="text-gray-100 sm:text-xl text-md text-center mb-2">Hackathon Day-2 </p>
           </div>
         </div>
         <div className="relative col-span-1 w-full h-full flex justify-center items-center">
@@ -83,7 +83,7 @@ const CustomizedTimeline = () => {
         <div data-aos="fade" className="col-span-4 w-full h-full ">
           <div className="w-full h-full bg-black border border-white p-4 rounded-md p-2 py-4 md:pl-4 border border-gradient-animation [#148ef9]">
             <h1 className="text-white text-xl text-center font-medium py-2">19 February 2024</h1>
-            <p className="text-gray-100 text-center sm:text-xl text-md mb-2">Prize Distribution</p>
+            <p className="text-gray-100 text-center sm:text-xl text-md mb-2">Prize Distribution </p>
           </div>
         </div>
       </div>
